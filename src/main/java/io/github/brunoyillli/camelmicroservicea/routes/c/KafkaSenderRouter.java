d7e163d440ff89663052cf61ed431b8900539bdf
brunoyillli <wolfteam1910@gmail.com>
2023-07-07 11:38:24 -0300

    adicionando arquivo para subir kafka e enviando json para o kakfa

---
+++ src/main/java/io/github/brunoyillli/camelmicroservicea/routes/c/KafkaSenderRouter.java
@@ -0,0 +1,17 @@
+package io.github.brunoyillli.camelmicroservicea.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaSenderRouter extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+
+		from("file:files/json")
+		.log("${body}")
+		.to("kafka:myKafkaTopic");
+	}
+
+}
