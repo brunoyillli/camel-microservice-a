c6d69faf09b936af90b617cb95255689cb247d85
brunoyillli <wolfteam1910@gmail.com>
2023-07-07 10:55:10 -0300

    enviando json and xml para o activemq

--- src/main/java/io/github/brunoyillli/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
+++ src/main/java/io/github/brunoyillli/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
@@ -8,10 +8,20 @@ public class ActiveMqSenderRouter extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		from("timer:active-mq-timer?period=10000")
-		.transform().constant("My message for Active MQ")
+//		enviar mensagem para o activemq a cada 10 segundos
+//		from("timer:active-mq-timer?period=10000")
+//		.transform().constant("My message for Active MQ")
+//		.log("${body}")
+//		.to("activemq:my-activemq-queue");
+		
+//		enviando arquivos json para o activemq
+//		from("file:files/json")
+//		.log("${body}")
+//		.to("activemq:my-activemq-queue");
+		
+		from("file:files/xml")
 		.log("${body}")
-		.to("activemq:my-activemq-queue");
+		.to("activemq:my-activemq-xml-queue");
 	}
 
 }
