c629c2461d850f0911537a9fa1584eaedde297d7
brunoyillli <wolfteam1910@gmail.com>
2023-07-08 20:14:19 -0300

    usando choise e simple language no camel nas rotas

--- src/main/java/io/github/brunoyillli/camelmicroservicea/routes/b/MyFileRouter.java
+++ src/main/java/io/github/brunoyillli/camelmicroservicea/routes/b/MyFileRouter.java
@@ -9,7 +9,17 @@ public class MyFileRouter extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("file:files/input")
-		.log("${body}")
+		.routeId("Files-Input-Route")
+		.transform().body(String.class)
+		.choice()
+			.when(simple("${file:ext} == 'xml'")) // ou poderia ser 'ends with'
+					.log("XML FILE")
+			.when(simple("${body} contains 'USD'"))
+				.log("not an XML FILE BUT contains USD")
+			.otherwise()
+				.log("NOT AN XML FILE")
+		.end()
+		.log("${messageHistory} ${file:absolute.path}") //https://camel.apache.org/components/3.20.x/languages/simple-language.html
 		.to("file:files/output");
 	}
 
