e7cfa6b85b576e20cb9ea38526709fca56afa29f
brunoyillli <wolfteam1910@gmail.com>
2023-07-10 21:18:07 -0300

    validacoes no choice usando beans

--- src/main/java/io/github/brunoyillli/camelmicroservicea/routes/b/MyFileRouter.java
+++ src/main/java/io/github/brunoyillli/camelmicroservicea/routes/b/MyFileRouter.java
@@ -1,11 +1,23 @@
 package io.github.brunoyillli.camelmicroservicea.routes.b;
 
+import java.util.Map;
+
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Handler;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyFileRouter extends RouteBuilder {
 
+	@Autowired
+	private DeciderBean deciderBean;
+	
 	@Override
 	public void configure() throws Exception {
 		from("file:files/input")
@@ -16,11 +28,32 @@ public class MyFileRouter extends RouteBuilder {
 					.log("XML FILE")
 			.when(simple("${body} contains 'USD'"))
 				.log("not an XML FILE BUT contains USD")
+			.when(method(deciderBean))
 			.otherwise()
 				.log("NOT AN XML FILE")
 		.end()
-		.log("${messageHistory} ${file:absolute.path}") //https://camel.apache.org/components/3.20.x/languages/simple-language.html
+		.to("direct://log-file-values")
 		.to("file:files/output");
+		
+		from("direct:log-file-values")
+		.log("${messageHistory} ${file:absolute.path}"); //https://camel.apache.org/components/3.20.x/languages/simple-language.html
+
 	}
 
+}
+
+
+@Component
+class DeciderBean{
+	
+	Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+	
+	public boolean isThisConditionMet(@Body String body,
+			@Headers Map<String, String> headers,
+			@ExchangeProperties Map<String, String> exchangeProperties) {
+		
+		logger.info("DeciderBean {} {} {}",body,headers,exchangeProperties);
+		
+		return true;
+	}
 }
